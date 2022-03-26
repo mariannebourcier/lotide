6463b1554f13f947159b330551d5badfc325dc64
mariannebourcier <marianne.bourcier@gmail.com>
2022-03-25 18:04:47 -0600

    eqArrays solution

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑🤑🤑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤨🤨🤨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+let eqArrays = function(firstArray, secondArray) {
+  if (JSON.stringify(firstArray) === JSON.stringify(secondArray)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
