e03eccfb2198ed7484d5b445a498f16439e7ea0f
mariannebourcier <marianne.bourcier@gmail.com>
2022-03-26 20:03:40 -0600

    without function created

---
+++ without.js
@@ -0,0 +1,44 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (JSON.stringify(firstArray) === JSON.stringify(secondArray)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// eslint-disable-next-line no-unused-vars
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`🤑🤑🤑 Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`🤨🤨🤨 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+
+//return an array without unwanted elements
+//take in source array and itemstoremove array
+//return elements from source that are not in the itemstoremove array
+//use assertArraysEqual for test-cases
+//return a new array and not modify the original
+
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i]);
+    }
+  }
+  return newArray;
+};
+
+
+
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
