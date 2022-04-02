cb20405402021faf1b241fdf12f57f6b5a7477a8
mariannebourcier <marianne.bourcier@gmail.com>
2022-04-02 12:55:09 -0600

    solution created

--- findKey.js
+++ findKey.js
@@ -1,20 +1,25 @@
 // create result variable
 
 // 	find key by value => returns array of given objects property names , all the strings
-// 	Object.keys
 // 	store the key => in a new variable
 // 	loop through the key array => to get the key we want when we use our callback
-// 	for of loop
+// 	for of loop => for in loop
 	
 // 	if the key is a match with the callback function => find the right key
 // 	push as the result variable => first match = push as result
-// 	else return undefined
+// 	break after finding the result else return undefined
 	
 // 	return result
 
 const findKey = function(object, callback) {
-  let result = []
-  
+  let result = [];
+
+  for (let key in object) {
+    if (callback(object[key])) {
+      result.push(key);
+      break;
+    }
+  } return result;
 };
 
 
@@ -22,14 +27,29 @@ const findKey = function(object, callback) {
 
 
 
-
-
-
-findKey({
+console.log(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2)); // => "noma"
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑🤑🤑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤨🤨🤨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
