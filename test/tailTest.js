4247496834df7e9dccf2139b33e9d846b428cc67
mariannebourcier <marianne.bourcier@gmail.com>
2022-04-11 17:24:45 -0600

    test

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,9 +3,9 @@ const tail = require('../tail');
 
 describe("#tail", () => {
   it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(tail([1, 2, 3]), 1);
+    assert.deepEqual(tail([1, 2, 3]), 1);
   });
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(tail(['5']), '5');
+    assert.deepEqual(tail(['5']), '5');
   });
 });
