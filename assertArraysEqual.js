25fa90531554b8a85c6d4c9cf95ef569308d59fb
mariannebourcier <marianne.bourcier@gmail.com>
2022-04-11 16:23:18 -0600

    refactored and testing file created

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,6 @@
-let eqArrays = function(firstArray, secondArray) {
-  if (JSON.stringify(firstArray) === JSON.stringify(secondArray)) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
-let assertArraysEqual = function(firstArray, secondArray) {
+const assertArraysEqual = function(firstArray, secondArray) {
   if (eqArrays(firstArray, secondArray)) {
     console.log(`🤑🤑🤑 Assertion Passed: ${firstArray} === ${secondArray}`);
   } else {
@@ -14,5 +8,4 @@ let assertArraysEqual = function(firstArray, secondArray) {
   }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2,3], [3,2,3]);
+module.exports = assertArraysEqual;
