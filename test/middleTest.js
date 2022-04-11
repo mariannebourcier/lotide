31651af69a1e8a92082a39efe15c122683fa031a
mariannebourcier <marianne.bourcier@gmail.com>
2022-04-11 17:24:15 -0600

    test

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,9 +4,9 @@ const assert = require('chai').assert;
 
 describe("#middle", () => {
   it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(middle([1, 2, 3]), 1);
+    assert.deepEqual(middle([1, 2, 3]), 1);
   });
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(middle(['5']), '5');
+    assert.deepEqual(middle(['5']), '5');
   });
 });
