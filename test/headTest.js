7213d02b2948406b47d17e2ce40929b6c65605d0
mariannebourcier <marianne.bourcier@gmail.com>
2022-04-11 16:44:44 -0600

    tested and refactored

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,12 @@
-const head = require('../head');
 
-//head
-console.log(head([5,6,7]), 5);
-console.log(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// eslint-disable-next-line space-in-parens
-console.log(head([]), );
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
